d5f0b46c49bb5f44940fe2dd9065ca10c04af639
Arifin <arifinsoemboed60.@gmail.com>
2023-01-17 11:40:52 +0700

    BELUMSELESE

--- src/UADID.java
+++ src/UADID.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class UADID {
+import java.util.ArrayList;
+
+public class UADID {
+    public String getMESSAGEID() {
+        return MESSAGEID;
+    }
+
+    public void setMESSAGEID(String MESSAGEID) {
+        this.MESSAGEID = MESSAGEID;
+    }
+
+    public String getMESSAGENAME() {
+        return MESSAGENAME;
+    }
+
+    public void setMESSAGENAME(String MESSAGENAME) {
+        this.MESSAGENAME = MESSAGENAME;
+    }
+
+    public ArrayList getCONTEXT() {
+        return CONTEXT;
+    }
+
+    public void setCONTEXT(ArrayList CONTEXT) {
+        this.CONTEXT = CONTEXT;
+
+    }
+
+    private String MESSAGEID;
+    private String MESSAGENAME;
+    private ArrayList CONTEXT;
 }
--- src/UAS.java
+++ src/UAS.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class UAS {
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class UAS {
+    public static void main(String[] args) {
+        int jum = 4;
+        UADID myMessage = new UADID();
+        Scanner sc = new Scanner(System.in);
+        myMessage.setMESSAGENAME(sc.nextLine());
+        ArrayList<String> context = new ArrayList<>();
+        for (int index=0; index<jum;index++){
+            context.add(sc.nextLine());
+        }
+        myMessage.setCONTEXT(context);
+        System.out.println("Kata yang dicari : ");
+        String cari = sc.nextLine();
+        System.out.println(myMessage.FindWords(context, cari));
+    }
 }
